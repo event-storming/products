a5fb3d7e15052e35414f4b045d9a3b111852ad8a
kimscott <sanaloveyou@uengine.org>
2019-12-10 17:00:57 +0900

    타임아웃 줌

--- src/main/java/com/example/template/ProductController.java
+++ src/main/java/com/example/template/ProductController.java
@@ -11,6 +11,14 @@ public class ProductController {
 
     @GetMapping("/product/{productId}")
     Product productStockCheck(@PathVariable(value = "productId") Long productId) {
+
+        System.out.println("productStockCheck call");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return  this.productService.getProductById(productId);
     }
 
